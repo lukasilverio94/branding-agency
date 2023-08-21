2a1406954e225840afd55de97e93672d86648e6f
lukasilverio94 <lukasilverio94@gmail.com>
2023-08-21 14:25:24 +0200

    added some responsiveness to mobile

--- src/components/BannerSection/index.jsx
+++ src/components/BannerSection/index.jsx
@@ -1,19 +1,22 @@
 import "./style.css";
 
 export default function BannerSection() {
+
+  const backgroundImageUrl = `${process.env.PUBLIC_URL}/assets/banner.png`;
+
   return (
     <section id="banner-container" className="bg-light-mode">
       <div
         className="img-banner"
-        style={{
-          backgroundImage: `url(${process.env.PUBLIC_URL + "/banner.png"})`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          width: "50%",
-          heigh: "100%",
-        }}
+         style={{
+           backgroundImage: {backgroundImageUrl},
+           backgroundRepeat: "no-repeat",
+           backgroundPosition: "center",
+           backgroundRepeat: "no-repeat",
+           backgroundSize: "cover",
+           width: "50%",
+           height: "100%",
+         }}
       >
         {/* banner img go here at css */}
       </div>
